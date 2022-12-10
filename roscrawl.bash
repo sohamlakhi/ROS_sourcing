5c3c1d27a411ecec7de33a4f7e77f109fa192c67
Soham Lakhi <sohamlakhi39@gmail.com>
2022-12-09 22:20:02 -0500

    commit to push all. Deleting OS

--- roscrawl.bash
+++ roscrawl.bash
@@ -2,26 +2,59 @@
 
 set -o posix #make bash file posix compliant for cross-compilation
 
+#set -e #terminate script if any command returns non-zero exit code
+
+# keep track of the last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+# echo an error message before exiting
+trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+
 #add crawler to opt/folder (find some place) and source from bashrc -> every time it is called, it must crawl through everything and ONLY add entry if it hasn't been added before
 
-#check if file exists
-function readfile() {
-	while 
+#check if file exists - boolean function (return exits the function and returns the exit code of the last statement
+function checkentry() {
+	while read LINE; do
+		if [ $LINE == $1 ]; then
+			false
+			return
+		fi
+	done < ~/.config/rossourcing/config.txt	
+	
+	echo toing
+	true
+	return
 }
+
+#CODE STARTS HERE
+
+#does config file exist?
 CONFIGFILE=~/.config/rossourcing/config.txt
-if ![ -f "$CONFIGFILE" ]; then
-    touch $CONFIGFILE
+
+if ! [ -d ~/.config/rossourcing/ ]; then
+	mkdir ~/.config/rossourcing
 fi
 
+if ! [ -f "$CONFIGFILE" ]; then
+	    touch $CONFIGFILE
+fi
+
+#crawling through opt for ros distros
 if [ -d /opt/ros ]; then 
+    cd /opt/ros/
     for DIRECTORY in *; do
     	if [ -d "$DIRECTORY" ]; then
-	    	if [ -f /opt/ros/$DIRECTORY/setup.bash ]; then 
-			#add key:value pair if it does not already exist			     	
+    		CONFIG_ENTRY=/opt/ros/$DIRECTORY/setup.bash
+    		#echo /opt/ros/$DIRECTORY/setup.bash
+	    	if [ -f "$CONFIG_ENTRY" ]; then 
+	    		if checkentry $CONFIG_ENTRY; then
+				echo "source $CONFIG_ENTRY" >> "$CONFIGFILE"
+			fi
 	    	fi
 	fi
     done
 fi
+
+#add workspaces using find ~ (use parameter substitution - like in while loop)
 
 #find all expressions matching _ws. and loop through them
 	#if director
@@ -31,4 +64,6 @@ fi
 
 #if file has trailing _ws? check if path/install/setup.bash exists? check if it has already been entered? add path/install/setup.bash
 
+#write read function. use parameter substitution 
 
+#use heredoc to touch and place code in different files (always checkfile) 
--- rosfunctions.bash
+++ rosfunctions.bash
@@ -2,6 +2,13 @@
 
 set -o posix #make bash file posix compliant for cross-compilation
 
+set -e #terminate script if any command returns non-zero exit code
+
+# keep track of the last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+# echo an error message before exiting
+trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+
 #crawl through directories to find ROS distros
 # ls /opt/ros | 
 
